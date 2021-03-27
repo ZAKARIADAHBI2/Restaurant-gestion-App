26a05e6d429ebf2623e83489647e4fc165bd543a
ZAKARIADAHBI2 <estfes22@gmail.com>
2021-03-27 14:34:56 +0100

    add

--- Pizza-Man-master/src/firebase/config.js
+++ Pizza-Man-master/src/firebase/config.js
@@ -3,11 +3,11 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCzoSkmU3tO1ShNTAn4qrwsLhNkrg_7wkw",
-    authDomain: "tryit-deb96.firebaseapp.com",
-    projectId: "tryit-deb96",
-    storageBucket: "tryit-deb96.appspot.com",
-    messagingSenderId: "926259819927",
-    appId: "1:926259819927:web:e95487752d0d83955380d2"
+    apiKey: "@",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   export default firebaseConfig;
